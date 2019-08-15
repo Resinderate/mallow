1ae01a41fef00cfc83e4adcb1af9cee525f6076c
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-08-15 23:04:00 +0100

    Adding error example.

--- src/example.py
+++ src/example.py
@@ -7,3 +7,5 @@ if __name__ == '__main__':
     print(tom)  # => <Member(first_name=tom)
     blob = s.dump(tom)
     print(blob)  # => {'firstName': 'tom'}
+
+    ronan = s.load({"firstName": "ronan"})  # => marshmallow.exceptions.ValidationError: {'firstName': ['Cannot start with R']}
